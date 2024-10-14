cfc8910b836e32f1dcb44079612431e44e502e34
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-10-14 20:29:22 -0300

    feat: adding save image button

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,13 @@ export function App() {
           <ul className="columns-2 sm:columns-3 lg:columns-5 pt-10 pb-20 md:py-30 gap-4">
             {imageList &&
               imageList.map((image: Image) => (
-                <li key={image.id} className="mb-4 break-inside-avoid">
+                <li key={image.id} className="mb-4 break-inside-avoid relative">
+                  <button className="bg-white/70 p-2 rounded-full absolute top-2 right-2 group">
+                    <Heart
+                      color="#111"
+                      className="transition-all duration-200 group-hover:stroke-red-500 group-hover:fill-red-500"
+                    />
+                  </button>
                   <img
                     src={image.download_url}
                     alt="image"
