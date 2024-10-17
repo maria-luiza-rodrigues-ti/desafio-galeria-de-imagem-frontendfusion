2ec8f581aa655a2c5497277c4ef753f7a07e1bbc
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-10-17 18:45:48 -0300

    feat: adding radix dialog to show image info

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,9 @@
 import { Heart } from "lucide-react";
 import { useContext, useEffect, useState } from "react";
-import { ImagesContext } from "../context/images-context";
+import * as Dialog from "@radix-ui/react-dialog";
 import { Toaster, toast } from "sonner";
+
+import { ImagesContext } from "../context/images-context";
 
 export interface ImageProps {
   id: string;
@@ -47,7 +49,10 @@ export function Gallery({ images }: { images: ImageProps[] }) {
     <ul className="columns-2 sm:columns-3 lg:columns-5 pt-10 pb-20 md:py-30 gap-4">
       {images &&
         localImages.map((image: ImageProps) => (
-          <li key={image.id} className="mb-4 break-inside-avoid relative">
+          <li
+            key={image.id}
+            className="mb-4 break-inside-avoid relative cursor-pointer"
+          >
             <button
               className="bg-white/70 p-2 rounded-full absolute top-2 right-2 group"
               onClick={() => handleSaveImage(image)}
@@ -59,11 +64,49 @@ export function Gallery({ images }: { images: ImageProps[] }) {
                 }`}
               />
             </button>
-            <img
-              src={image.download_url}
-              alt="image"
-              className="w-full object-cover rounded-lg"
-            />
+
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <img
+                  src={image.download_url}
+                  className="w-full object-cover rounded-lg"
+                />
+              </Dialog.Trigger>
+              <Dialog.Portal>
+                <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
+                <Dialog.Content asChild>
+                  <figure className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-background bg-white rounded-2xl h-[70vh] w-[70vh] flex gap-2 overflow-hidden">
+                    <img
+                      src={image.download_url}
+                      alt=""
+                      className="w-1/2 object-cover"
+                    />
+                    <Dialog.Close />
+
+                    <Dialog.Description asChild>
+                      <div className="flex-1 flex flex-col justify-center gap-4 pr-2">
+                        <div>
+                          <span className="text-text-color text-left text-sm">
+                            Autor:
+                          </span>
+                          <Dialog.Title className="text-3xl font-bold">
+                            {image.author}
+                          </Dialog.Title>
+                        </div>
+                        <div>
+                          <span className="block text-sm">
+                            Largura: {image.width}
+                          </span>
+                          <span className="block text-sm">
+                            Altura: {image.height}
+                          </span>
+                        </div>
+                      </div>
+                    </Dialog.Description>
+                  </figure>
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
           </li>
         ))}
       <Toaster richColors />
